32c96909731907b9f661ad5f471a9b43744f1318
Silvia Bailman <silaba70@gmail.com>
2023-02-11 15:25:41 -0300

    Carga de proyecto cv

---
+++ Proyectofinal/acciones.js
@@ -0,0 +1,18 @@
+
+
+function direccion() {
+    document.getElementById("demo").innerHTML = "Av. Thames 2565";
+  }
+
+function telefono() {
+    document.getElementById("demo").innerHTML = "1158-258-578";
+  }
+
+function mail() {
+    document.getElementById("demo").innerHTML = "mimail@correo.com";
+  }
+
+function link() {
+    document.getElementById("demo").innerHTML = "www.milinkedin.com";
+  }
+
